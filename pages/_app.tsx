cbeb140adef38f5145b8883b1aab965e442ba6c8
xgeovanedamasceno <xgeovanedamasceno>
2022-02-20 21:36:13 -0300

    feat(header-main): add header and main elements
    Add header and main elements to 'pages/_app' and sets tailwind class
    to style them.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
+import Link from 'next/link'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,7 +11,18 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+
+      <header className='py-10 bg-gradient-to-r from-green-400 to-blue-500 text-center'>
+        <Link href="/">
+          <a>
+            <h2 className='text-5xl font-bold text-white'>Meu Blog</h2>
+          </a>
+        </Link>
+      </header>
+
+      <main className='my-6 mx-auto p-6 bg-white sm-shadow-lg rounded prose lg:prose-xl'>
+        <Component {...pageProps} />
+      </main>
     </>
   )
 }
