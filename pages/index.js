9550bc176a8a14b61d0efc9a0a19a530fd9e4710
xgeovanedamasceno <xgeovanedamasceno>
2022-02-20 22:16:26 -0300

    feat(post): add blog post to initial page
    Create file 'lib/post.js' that will be responsible for reading markdown
    files and return a list of posts.
    Add post to initial page by rendering the return of getPosts function.
    Alter file extension to 'js' temporarly because it's causing bug at item
    list key.

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import getPosts from '../lib/posts'
+
+const Blog = ({ posts }) => {
+  return (
+    <>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(({ slug, title }) => (
+          <li key={slug}>
+            <Link href={`/${slug}`}>
+              <a>{ title }</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: await getPosts(),
+    },
+  };
+}
+
+export default Blog
--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-
-const Blog: NextPage = () => {
-  return <h1>Posts</h1>;
-}
-
-export default Blog
