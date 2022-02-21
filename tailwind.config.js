380fe829a31bba00cc9c2d46af0ed04de88d7fc1
xgeovanedamasceno <xgeovanedamasceno>
2022-02-20 21:40:31 -0300

    chore: add typography style by loading tailwindcss plugin
    Load pattern and behavior through official typography plugin from
    '@tailwindcss/typography'

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: "#3182ce",
+              "&:hover": {
+                color: "#2c5282",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
